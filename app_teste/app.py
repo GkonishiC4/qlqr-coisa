eaca22f35760e2ba3abc07b2080a634ed9d2ab7a
guilhemekonishi <guilhermekonishi35@gmail.comgit config --global user.email>
2023-05-02 19:20:01 -0300

    first commit

---
+++ app_teste/app.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+
+
+# configuração do Flask-Mail para enviar e-mails
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'guilhemeappflow@gmail.com'
+app.config['MAIL_PASSWORD'] = 'G#7355608g'
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USE_TLS'] = False
+mail = Mail(app)
+
+# rota de index
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# rota de cadastro de usuário
+class RegistrationForm(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Senha', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirmar Senha', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Cadastrar')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash('Cadastro bem-sucedido! Faça login para continuar.', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+# rota de login de usuário
+class LoginForm(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Senha', validators=[DataRequired()])
+    submit = SubmitField('Entrar')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login bem-sucedido!', 'success')
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
+
+# rota de redefinição de senha
+class ResetPasswordForm(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    submit = SubmitField('Enviar E-mail para Redefinição')
+
+@app.route('/reset_password', methods=['GET', 'POST'])
+def reset_password():
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user_email = form.email.data
+        msg = Message('Redefinir Senha', sender='seu_email_aqui@gmail.com', recipients=[user_email])
+        msg.body = f"Clique no seguinte link para redefinir sua senha: {url_for('reset_password_confirm', _external=True)}"
+        mail.send(msg)
+        flash('E-mail de redefinição de senha enviado!', 'success')
+        return redirect(url_for('login'))
+    return render_template('reset_password.html', form=form)
+
+@app.route('/reset_password_confirm')
+def reset_password_confirm():
+    return render_template('reset_password_confirm.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
