21f2908f3a663904f52f38b9f3a924b8926bb1da
guilhemekonishi <guilhermekonishi35@gmail.comgit config --global user.email>
2023-05-04 19:11:20 -0300

    att4

--- app_teste/app.py
+++ app_teste/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField
 from wtforms.validators import DataRequired, Email, EqualTo
 from flask_mail import Mail, Message
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, SubmitField, validators
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
@@ -12,7 +15,7 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'guilhemeappflow@gmail.com'
-app.config['MAIL_PASSWORD'] = 'G#7355608g'
+app.config['MAIL_PASSWORD'] = 'oghobneztdbikirt'
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USE_TLS'] = False
 mail = Mail(app)
@@ -25,8 +28,12 @@ def index():
 # rota de cadastro de usuário
 class RegistrationForm(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Email()])
-    password = PasswordField('Senha', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirmar Senha', validators=[DataRequired(), EqualTo('password')])
+    password = PasswordField('Senha', validators=[DataRequired(message='insira uma senha'),
+                                                  EqualTo('confirma', message='A confirmação de senha é inválida'),
+                                                  validators.Length(min=6, message='A senha deve ter pelo menos 6 caracteres')])
+    confirm_password = PasswordField('Confirmar Senha', validators=[DataRequired(message='insira uma senha'),
+                                                  EqualTo('confirma', message='A confirmação de senha é inválida'),
+                                                  validators.Length(min=6, message='A senha deve ter pelo menos 6 caracteres')])
     submit = SubmitField('Cadastrar')
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -61,7 +68,7 @@ def reset_password():
     form = ResetPasswordForm()
     if form.validate_on_submit():
         user_email = form.email.data
-        msg = Message('Redefinir Senha', sender='seu_email_aqui@gmail.com', recipients=[user_email])
+        msg = Message('Redefinir Senha', sender='guilhemeappflow@gmail.com', recipients=[user_email])
         msg.body = f"Clique no seguinte link para redefinir sua senha: {url_for('reset_password_confirm', _external=True)}"
         mail.send(msg)
         flash('E-mail de redefinição de senha enviado!', 'success')
